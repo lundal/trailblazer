0285aaeed534d1b717046d94342508b9f6e6a496
Per Thomas Lundal <perthomas@gmail.com>
2015-05-07 15:30:15 +0200

    Use multiple files with index

--- d20pfsrd/feats.py
+++ d20pfsrd/feats.py
@@ -1,24 +1,38 @@
 #!/bin/python
 
-print("\"feats\":[")
+index = []
 
-with open("feats.txt") as file:
+def processEntry(line):
+    data = line.replace("\"", "\\\"").split("\t")
+    path = "feats/{:0>4d}.json".format(len(index))
+    with open(path, 'w') as outfile:
+        index.append(data[1]);
+        outfile.write("{\n")
+        outfile.write("\"name\":\"" + data[1] + "\",\n")
+        outfile.write("\"type\":\"" + data[2] + "\",\n")
+        outfile.write("\"description\":\"" + data[3] + "\",\n")
+        outfile.write("\"prequesites\":\"" + data[4] + "\",\n")
+        outfile.write("\"benefit\":\"" + data[6] + "\",\n")
+        outfile.write("\"normal\":\"" + data[7] + "\",\n")
+        outfile.write("\"special\":\"" + data[8] + "\"\n") # No comma
+        outfile.write("}\n")
+
+def writeIndex():
+    path = "feats/index.json"
+    with open(path, 'w') as outfile:
+        outfile.write("{\n")
+        outfile.write("\"feats\":[\n")
+        for entry in index[0:-1]:
+            outfile.write("\"" + entry + "\",\n")
+        outfile.write("\"" + index[-1] + "\"\n")
+        outfile.write("]\n")
+        outfile.write("}\n")
+
+with open("feats.txt") as infile:
     first = True
-    for line in file:
-        data = line.replace("\"", "\\\"").split("\t")
+    for line in infile:
         if first == True:
             first = False
             continue
-        print("{")
-        print("\"name\":\"" + data[1] + "\",")
-        print("\"type\":\"" + data[2] + "\",")
-        print("\"description\":\"" + data[3] + "\",")
-        print("\"prequesites\":\"" + data[4] + "\",")
-        print("\"benefit\":\"" + data[6] + "\",")
-        print("\"normal\":\"" + data[7] + "\",")
-        print("\"special\":\"" + data[8] + "\",")
-        print("},")
-
-print("],")
-
-# TODO: Index
+        processEntry(line)
+    writeIndex()
