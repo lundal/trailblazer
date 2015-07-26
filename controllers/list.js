ef2540b5e86792c94160717e9b0fe70332601f8e
Per Thomas Lundal <perthomas@gmail.com>
2015-07-26 15:09:24 +0200

    Further pimp and refactor list

--- controllers/list.js
+++ controllers/list.js
@@ -8,9 +8,18 @@ function($scope, $location,
         $location.path('character/local/' + guid);
     };
 
-    $scope.localDeleteAll = function() {
-        characterService.deleteAll(localStorageService, function(success) {});
-        $scope.localCharacters = [];
+    $scope.localOpen = function(character) {
+        var guid = character.guid;
+        $location.path('character/local/' + guid);
+    };
+
+    $scope.localDelete = function(character, $event) {
+        $event.stopPropagation();
+
+        if (doubleClickService.click(character) == doubleClickService.doubleClick) {
+            characterService.delete(localStorageService, character.guid, function(success) {});
+            removeItem($scope.localCharacters, character);
+        }
     };
 
     var localInit = function() {
@@ -40,9 +49,18 @@ function($scope, $location,
         $location.path('character/drive/' + guid);
     };
 
-    $scope.driveDeleteAll = function() {
-        characterService.deleteAll(driveStorageService, function(success) {});
-        $scope.driveCharacters = [];
+    $scope.driveOpen = function(character) {
+        var guid = character.guid;
+        $location.path('character/drive/' + guid);
+    };
+
+    $scope.driveDelete = function(character, $event) {
+        $event.stopPropagation();
+
+        if (doubleClickService.click(character) == doubleClickService.doubleClick) {
+            characterService.delete(driveStorageService, character.guid, function(success) {});
+            removeItem($scope.driveCharacters, character);
+        }
     };
 
     $scope.driveRetry = function() {
@@ -104,47 +122,19 @@ function($scope, $location,
         alert('Todo: Export');
     };
 
-    $scope.openCharacterLocal = function(character) {
-        var guid = character.guid;
-        $location.path('character/local/' + guid);
-    };
-
-    $scope.openCharacterDrive = function(character) {
-        var guid = character.guid;
-        $location.path('character/drive/' + guid);
+    $scope.deleteStyle = function(character) {
+        if (doubleClickService.wasRecentlyClicked(character)) {
+            return 'button-recently-clicked';
+        }
+        else {
+            return '';
+        }
     };
 
     var removeItem = function(list, item) {
         var index = list.indexOf(item);
         if (index != -1) {
             list.splice(index, 1);
-        }
-    };
-
-    $scope.deleteCharacterLocal = function(character, $event) {
-        $event.stopPropagation();
-
-        if (doubleClickService.click(character) == doubleClickService.doubleClick) {
-            characterService.delete(localStorageService, character.guid, function(success) {});
-            removeItem($scope.localCharacters, character);
-        }
-    };
-
-    $scope.deleteCharacterDrive = function(character, $event) {
-        $event.stopPropagation();
-
-        if (doubleClickService.click(character) == doubleClickService.doubleClick) {
-            characterService.delete(driveStorageService, character.guid, function(success) {});
-            removeItem($scope.driveCharacters, character);
-        }
-    };
-
-    $scope.deleteCharacterStyle = function(character) {
-        if (doubleClickService.wasRecentlyClicked(character)) {
-            return 'button-recently-clicked';
-        }
-        else {
-            return '';
         }
     };
 
